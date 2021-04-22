2cd49627e4db6ff99356cb4ba714b27454a5af4f
João Matos <jvdematos004@gmail.com>
2021-04-21 23:16:21 -0300

    Add files via upload

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,17 +1,18 @@
-import format from 'date-fns/format'
-import ptBR from 'date-fns/locale/pt-BR' //importando o ptBR
-import styles from './styles.module.scss'
+import format from 'date-fns/format';
+import ptBR from 'date-fns/locale/pt-BR';
+
+import styles from './styles.module.scss';
 
 export function Header() {
-  const currentData = format(new Date(),'EEEEEE, d MMMM', {
-  
-  locale:ptBR,
-  })
-return(
-  <header className={styles.HeaderContainer}>
-    <img src="/logo.svg" alt="podcastr" />
-    <p>O melhor para você ouvir, sempre</p>
-    <span>{currentData}</span>
-  </header>
-);
+    const currentDate = format(new Date(), 'EEEEEE, d MMMM', {
+        locale: ptBR,
+    });
+
+    return (
+        <header className={styles.headerContainer}>
+            <img src="/logo.svg" alt="Podcastr"/>
+            <p>O melhor para você ouvir, sempre</p>
+            <span>{currentDate}</span>
+        </header>
+    )
 }
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,20 +1,20 @@
-import { Header } from '../components/Header';
 import '../styles/global.scss';
 
+import { Header } from "../components/Header";
+import { Player } from "../components/Player";
+
 import styles from '../styles/app.module.scss';
-import { Player } from '../components/Player';
 
-
-  function MyApp({ Component, pageProps }) {
+function MyApp({ Component, pageProps }) {
   return (
-    <div className={ styles.wraper }>
-       <main> 
-      <Header />
-      <Component {...pageProps} />
+    <div className={styles.wrapper}>
+      <main>
+        <Header />
+        <Component {...pageProps} />
       </main>
       <Player />
     </div>
-
   )
 }
+
 export default MyApp
